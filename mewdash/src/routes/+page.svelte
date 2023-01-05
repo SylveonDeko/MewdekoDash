f54597c755276f45c2eac5b6601ae18cbe437528
Dominik Lenz <dominik@notabot.cc>
2023-01-05 20:54:06 +0100

    adds example component

--- mewdash/src/routes/+page.svelte
+++ mewdash/src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
-<div class="flex flex-row">
+<div class="flex flex-col">
     <p>Visit <a href="https://kit.svelte.dev" class="text-rose-600">kit.svelte.dev</a> to read the documentation</p>
+    <Button buttonName="Some Button"/>
 </div>
 
-
+<script>
+    import Button from "$lib/Button.svelte";
+</script>
