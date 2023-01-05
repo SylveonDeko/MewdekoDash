3a245f4aed7373894bcb59e68a4eef2a05ba4722
Dominik Lenz <dominik@notabot.cc>
2023-01-05 20:29:24 +0100

    adds tailwind

--- mewdash/src/routes/+page.svelte
+++ mewdash/src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-row">
+    <p>Visit <a href="https://kit.svelte.dev" class="text-rose-600">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+
