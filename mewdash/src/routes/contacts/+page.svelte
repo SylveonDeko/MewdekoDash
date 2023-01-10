02d31c5e907bb67c36183deab413480f0928bc76
SylveonDeko <sylveon@mewdeko.tech>
2023-01-10 18:58:13 -0500

    Format code, fix navbar not working, maybe unbootstrapping soon

---
+++ mewdash/src/routes/contacts/+page.svelte
@@ -0,0 +1,84 @@
+<sveltekit:head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport">
+    <title>Contacts - Mewdeko</title>
+    <meta content="#938018" name="theme-color">
+    <meta content="The only completely free bot that's both open source and completely customizable."
+          name="description">
+    <meta content="Invite Mewdeko! The only completely free bot that's both open source and completely customizable."
+          property="og:description">
+    <meta content="website" property="og:type">
+    <meta content="Mewdeko - The most customizable discord bot." name="twitter:title">
+    <meta content="summary_large_image" name="twitter:card">
+    <meta content="https://mewdeko.tech/img/monogatari-series-background-hd-1600x900-108924-1.jpg" name="twitter:image">
+    <meta content="Invite Mewdeko! The only completely free bot that's both open source and completely customizable."
+          name="twitter:description">
+    <meta content="https://mewdeko.tech/img/monogatari-series-background-hd-1600x900-108924-1.jpg" property="og:image">
+    <meta content="Mewdeko - The most customizable discord bot." property="og:title">
+    <link href="img/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" type="image/png">
+    <link href="img/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png">
+    <link href="img/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png">
+    <link href="img/apple-touch-icon.png" rel="icon" sizes="180x180" type="image/png">
+    <link href="img/android-chrome-192x192.png" rel="icon" sizes="192x192" type="image/png">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap"
+          rel="stylesheet">
+    <link href="css/Navbar-Right-Links-Dark-icons.css" rel="stylesheet">
+</sveltekit:head>
+
+<sveltekit:body>
+    <nav class="navbar navbar-dark navbar-expand-md bg-dark py-3" style="background: rgb(45,44,56);">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"><span
+                class="bs-icon-md bs-icon-circle bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"
+                style="background: rgb(120, 101, 0);"><img alt="Mewdeko Avatar" class="rounded-circle img-fluid" height="47"
+                                                           src="https://cdn.mewdeko.tech/Mewdeko.png"
+                                                           width="43"></span><span>Mewdeko</span></a>
+            <button class="navbar-toggler" data-bs-target="#navcol-5" data-bs-toggle="collapse"><span
+                    class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse text-center" id="navcol-5">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="/contacts">Contact Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/staff">Staff</a></li>
+                    <li class="nav-item"><a class="nav-link" href="resources">Resources</a></li>
+                </ul>
+                <a class="btn btn-primary ms-md-2" href="https://discord.com/oauth2/authorize?client_id=752236274261426212&amp;scope=bot&amp;permissions=66186303&amp;scope=bot"
+                   role="button"
+                   style="border-style: none;border-color: var(--bs-card-cap-bg);border-top-color: rgb(255,255,255);background: rgb(147,128,24);"
+                   target="_blank">Invite Me</a>
+            </div>
+        </div>
+    </nav>
+    <section class="py-5 mt-5">
+        <div class="container py-5">
+            <div class="row mb-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <p class="fw-bold text-success mb-2">Contacts</p>
+                    <h2 class="fw-bold">How you can reach us</h2>
+                    <svg fill="currentColor" height="1em" style="font-size: 39px;" viewBox="0 -64 640 640"
+                         width="1em" xmlns="http://www.w3.org/2000/svg">
+                        <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. -->
+                        <path d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"></path>
+                    </svg>
+                    <p><a href="https://discord.gg/mewdeko" style="color: rgb(255,255,255);" target="_blank">https://discord.gg/mewdeko</a>
+                    </p>
+                    <svg fill="currentColor" height="1em" style="font-size: 43px;" viewBox="0 0 512 512"
+                         width="1em" xmlns="http://www.w3.org/2000/svg">
+                        <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. -->
+                        <path d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z"></path>
+                    </svg>
+                    <p><a href="mailto:sylveon@mewdeko.tech" style="color: rgb(255,255,255);">sylveon@mewdeko.tech</a>
+                    </p>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-4 col-xl-4 d-flex justify-content-center justify-content-xl-start">
+                    <div class="d-flex flex-wrap flex-md-column justify-content-md-start align-items-md-start h-100"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/bs-init.js"></script>
+    <script src="js/bold-and-dark.js"></script>
+    <script src="js/untitled.js"></script>
+</sveltekit:body>
---
+++ mewdash/src/routes/staff/+page.svelte
@@ -0,0 +1,95 @@
+<sveltekit:head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport">
+    <title>Team - Mewdeko</title>
+    <meta content="#938018" name="theme-color">
+    <meta content="The only completely free bot that's both open source and completely customizable."
+          name="description">
+    <meta content="Invite Mewdeko! The only completely free bot that's both open source and completely customizable."
+          property="og:description">
+    <meta content="website" property="og:type">
+    <meta content="Mewdeko - The most customizable discord bot." name="twitter:title">
+    <meta content="summary_large_image" name="twitter:card">
+    <meta content="https://mewdeko.tech/img/monogatari-series-background-hd-1600x900-108924-1.jpg" name="twitter:image">
+    <meta content="Invite Mewdeko! The only completely free bot that's both open source and completely customizable."
+          name="twitter:description">
+    <meta content="https://mewdeko.tech/img/monogatari-series-background-hd-1600x900-108924-1.jpg" property="og:image">
+    <meta content="Mewdeko - The most customizable discord bot." property="og:title">
+    <link href="img/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" type="image/png">
+    <link href="img/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png">
+    <link href="img/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png">
+    <link href="img/apple-touch-icon.png" rel="icon" sizes="180x180" type="image/png">
+    <link href="img/android-chrome-192x192.png" rel="icon" sizes="192x192" type="image/png">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap"
+          rel="stylesheet">
+    <link href="css/Navbar-Right-Links-Dark-icons.css" rel="stylesheet">
+</sveltekit:head>
+
+<sveltekit:body>
+    <nav class="navbar navbar-dark navbar-expand-md bg-dark py-3" style="background: rgb(45,44,56);">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"><span
+                class="bs-icon-md bs-icon-circle bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"
+                style="background: rgb(120, 101, 0);"><img alt="Mewdeko Avatar" class="rounded-circle img-fluid" height="47"
+                                                           src="https://cdn.mewdeko.tech/Mewdeko.png"
+                                                           width="43"></span><span>Mewdeko</span></a>
+            <button class="navbar-toggler" data-bs-target="#navcol-5" data-bs-toggle="collapse"><span
+                    class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse text-center" id="navcol-5">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="/contacts">Contact Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/staff">Staff</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/resources">Resources</a></li>
+                </ul>
+                <a class="btn btn-primary ms-md-2" href="https://discord.com/oauth2/authorize?client_id=752236274261426212&amp;scope=bot&amp;permissions=66186303&amp;scope=bot"
+                   role="button"
+                   style="border-style: none;border-color: var(--bs-card-cap-bg);border-top-color: rgb(255,255,255);background: rgb(147,128,24);"
+                   target="_blank">Invite Me</a>
+            </div>
+        </div>
+    </nav>
+    <section class="py-5">
+        <div class="container py-5">
+            <div class="row mb-4 mb-lg-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <p class="fw-bold text-success mb-2">Our Staff</p>
+                    <h2 class="fw-bold">Major People</h2>
+                    <p class="text-muted">The team of Mewdeko, strong I would say.</p>
+                </div>
+            </div>
+            <div class="row row-cols-2 row-cols-md-3 text-center mx-auto" style="max-width: 700px;">
+                <div class="col mb-4">
+                    <div class="text-center"><img alt="Owner/Developer SylveonDeko" class="rounded-circle img-fluid mb-3 fit-cover"
+                                                  height="150"
+                                                  src="https://cdn.discordapp.com/avatars/280835732728184843/a_7c7edfc05cfa28c83706a7c53da77d0a.gif?size=2048"
+                                                  width="150">
+                        <h5 class="fw-bold mb-0"><strong>SylveonDeko</strong></h5>
+                        <p class="text-muted mb-2">Owner/Developer</p>
+                    </div>
+                </div>
+                <div class="col text-center mb-4">
+                    <div class="text-center"><img alt="Owner/Developer GraveyardDwellingCat" class="rounded-circle img-fluid mb-3 fit-cover"
+                                                  height="150"
+                                                  src="https://cdn.discordapp.com/avatars/786375627892064257/9d66731831a26f7960198934d8f36d19.png"
+                                                  width="150">
+                        <h6 class="fw-bold text-center mb-0"><strong>GraveyardDwellingCat</strong></h6>
+                        <p class="text-muted mb-2">Owner/Developer</p>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="text-center"><img alt="Owner/Major Suport Paw" class="rounded-circle img-fluid mb-3 fit-cover"
+                                                  height="150"
+                                                  src="https://cdn.discordapp.com/avatars/436201394543001600/a_413cad5a6215188495d259ac8742b575.gif?size=512"
+                                                  width="150">
+                        <h5 class="fw-bold mb-0"><strong>Paw</strong></h5>
+                        <p class="text-muted mb-2">Owner/Major Supporter</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/bs-init.js"></script>
+    <script src="js/bold-and-dark.js"></script>
+    <script src="js/untitled.js"></script>
+</sveltekit:body>
