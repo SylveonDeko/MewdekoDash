fa5cd15fddb2a3d06a3f6b073ffa9d2b02db2855
Dominik Lenz <dominik@notabot.cc>
2023-01-12 17:06:14 +0100

    adds remaining splits

--- mewdash/src/lib/util/FluidElement.svelte
+++ mewdash/src/lib/util/FluidElement.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    const sizes = ["1/2", "1/3", "1/4"]
+    const sizes = ["1/2", "1/3", "1/4", "1/5", "1/6"]
 
     export let breakpoints = ["md", "lg", "xl"];
     export let maxBreaks: number = sizes.length;
@@ -7,8 +7,6 @@
     const widths = Array.from(Array(Math.min(maxBreaks - 1, sizes.length, breakpoints.length)).keys()) //maxBreaks - 1 because of 0 index
         .map((i) => `${breakpoints[i]}:w-${sizes[i]}`)  //Go  and map that to ascending splits
         .join(" ") //join with space
-
-    console.log(widths)
 </script>
 
 <div class="w-full {widths} flex justify-center">
