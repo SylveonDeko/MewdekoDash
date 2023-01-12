05d5a61a183033a4331118747251ea3f07081cca
Dominik Lenz <dominik@notabot.cc>
2023-01-12 13:27:28 +0100

    suppresses wrong unused default export

--- mewdash/postcss.config.js
+++ mewdash/postcss.config.js
@@ -2,6 +2,7 @@ import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
 import tailwindConfig from './tailwind.config.js'
 
+// noinspection JSUnusedGlobalSymbols
 export default {
     plugins: [tailwind(tailwindConfig), autoprefixer]
 }
