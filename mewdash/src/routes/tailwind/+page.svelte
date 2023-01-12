f97b3ee7738ab006865e1832ee11c69b3d1a26cf
Dominik Lenz <dominik@notabot.cc>
2023-01-12 19:56:01 +0100

    adds head

--- mewdash/src/routes/tailwind/+page.svelte
+++ mewdash/src/routes/tailwind/+page.svelte
@@ -4,6 +4,28 @@
     import CommentElement from "$lib/CommentElement.svelte";
 </script>
 
+<svelte:head>
+    <title>Home - Mewdeko</title>
+    <meta content="#938018" name="theme-color">
+    <meta content="The only completely free bot that's both open source and completely customizable."
+          name="description">
+    <meta content="Invite Mewdeko! The only completely free bot that's both open source and completely customizable."
+          property="og:description">
+    <meta content="website" property="og:type">
+    <meta content="Mewdeko - The most customizable discord bot." name="twitter:title">
+    <meta content="summary_large_image" name="twitter:card">
+    <meta content="https://mewdeko.tech/img/monogatari-series-background-hd-1600x900-108924-1.jpg" name="twitter:image">
+    <meta content="Invite Mewdeko! The only completely free bot that's both open source and completely customizable."
+          name="twitter:description">
+    <meta content="https://mewdeko.tech/img/monogatari-series-background-hd-1600x900-108924-1.jpg" property="og:image">
+    <meta content="Mewdeko - The most customizable discord bot." property="og:title">
+    <link href="img/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" type="image/png">
+    <link href="img/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png">
+    <link href="img/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png">
+    <link href="img/apple-touch-icon.png" rel="icon" sizes="180x180" type="image/png">
+    <link href="img/android-chrome-192x192.png" rel="icon" sizes="192x192" type="image/png">
+</svelte:head>
+
 <header class="py-12 px-4 sm:px-12 flex justify-center bg-slate-200">
     <h1 class="max-w-[70rem] font-extrabold text-center text-[28px] sm:text-3xl lg:text-4xl xl:text-[40px]">
         The most&nbsp;customizable free&nbsp;bot you'll&nbsp;find. Open&nbsp;Source&nbsp;too.
