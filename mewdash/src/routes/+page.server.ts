b3fac73bc916e6f4514eca1fee1abd0fb1df65ea
Dominik Lenz <dominik@notabot.cc>
2023-01-16 16:15:43 +0100

    adds type to +page.server.ts

--- mewdash/src/routes/+page.server.ts
+++ mewdash/src/routes/+page.server.ts
@@ -1,18 +1,20 @@
 import {TopGGKey} from "$lib/server/secrets";
+import type {PageServerLoad} from "./$types"
 
-export async function load(): Promise<{ servercount: number | null }> {
+export const load: PageServerLoad = async (): Promise<{ servercount: number | null }> => {
     return fetch("https://top.gg/api/bots/752236274261426212/stats", {
-      method: "get",
-      headers: new Headers({
-        'Authorization':  TopGGKey
-      })})
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        return { servercount: parseInt(data.server_count) };
-      })
-      .catch(() =>{
-        return { servercount: null };
-      });
+        method: "get",
+        headers: new Headers({
+            'Authorization': TopGGKey
+        })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            return {servercount: parseInt(data.server_count)};
+        })
+        .catch(() => {
+            return {servercount: null};
+        });
 }
