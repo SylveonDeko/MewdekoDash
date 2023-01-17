2ce265ea562705f52ef369530961bdf5939facb2
Dominik Lenz <dominik@notabot.cc>
2023-01-17 10:53:28 +0100

    fixes nav

--- mewdash/src/lib/nav/Navbar.svelte
+++ mewdash/src/lib/nav/Navbar.svelte
@@ -1,14 +1,12 @@
 <nav class="py-4 bg-mewd-dark-grey">
     <div class="container flex flex-wrap items-center justify-between mx-auto px-6">
-        <a class="flex items-center mr-4 py-[0.3rem]" href="/">
-            <img alt="Mewdeko Avatar" class="h-12 mr-3" height="48" width="48" src="img/cdn/Mewdeko.png"/>
-            <span class="self-center text-xl font-semibold whitespace-nowrap text-mewd-white">Mewdeko</span>
+        <a title="mewdeko-banner" class="flex items-center mr-4 py-[0.3rem]" href="/">
+            <img alt="Mewdekos Avatar" class="h-12 mr-3" height="48" width="48" src="img/cdn/Mewdeko.png"/>
+            <span aria-hidden="true" class="self-center text-xl font-semibold whitespace-nowrap text-mewd-white">Mewdeko</span>
         </a>
         <!--suppress HtmlWrongAttributeValue -->
-        <button aria-controls="mobile-navbar"
-                aria-expanded="{showMenu?'true':'false'}"
-                on:click={toggleNavbar}
-                class="inline-flex items-center px-3 py-1 rounded-3xl border border-px border-transparent hover:border-mewd-light-transparent md:hidden" >
+        <button class="inline-flex items-center px-3 py-1 rounded-3xl border border-px border-transparent hover:border-mewd-light-transparent md:hidden"
+                aria-controls="mobile-navbar" aria-expanded="{showMenu?'true':'false'}" on:click={toggleNavbar}>
             <span class="sr-only">open nav-menu</span>
             <svg class="h-8 stroke-mewd-transparent" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4 7h22M4 15h22M4 23h22" stroke-linecap="round" stroke-miterlimit="10"
@@ -16,20 +14,18 @@
             </svg>
         </button>
         {#if showMenu}
-            <div id="mobile-navbar" class="w-full md:hidden" transition:slide={{ duration: 300 }}>
-                <ul class="flex flex-col p-4 mt-4">
-                    {#each items as item}
-                        <Link href="{item.href}" title="{item.title}" current="{isCurrentPage(item.href)}"/>
-                    {/each}
-                </ul>
-            </div>
-        {/if}
-        <div class="hidden md:block w-auto" id="desktop-navbar">
-            <ul class="flex flex-row p-4 space-x-8 text-[16px] font-medium border-0">
+            <div class="md:hidden w-full  flex flex-col p-4 mt-4" transition:slide={{ duration: 300 }}
+                 id="mobile-navbar" title="pages nav">
                 {#each items as item}
                     <Link href="{item.href}" title="{item.title}" current="{isCurrentPage(item.href)}"/>
                 {/each}
-            </ul>
+            </div>
+        {/if}
+        <div class="hidden md:flex w-auto flex-row p-4 space-x-8 text-[16px] font-medium border-0"
+             id="desktop-navbar" title="pages nav">
+            {#each items as item}
+                <Link href="{item.href}" title="{item.title}" current="{isCurrentPage(item.href)}"/>
+            {/each}
         </div>
     </div>
 </nav>
