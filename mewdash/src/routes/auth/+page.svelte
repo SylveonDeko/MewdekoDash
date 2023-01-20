ace43c89acffb17dbfb1600f98d02a3eaef78d8a
SylveonDeko <sylveon@mewdeko.tech>
2023-01-19 19:09:57 -0500

    fix auth

--- mewdash/src/routes/auth/+page.svelte
+++ mewdash/src/routes/auth/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="bg-mewd-light-grey h-screen">
-    <h1 class="text-center"><br><br>Auth Code Receiver</h1>
+    <h1 class="text-3xl text-center text-mewd-white"><br><br>Auth Code Receiver</h1>
     {#if data.code}
         <h3 class="text-center text-mewd-white text-2xl">Your Discord Auth Code is&nbsp;<code
           style="color: rgb(147,128,24);">{data.code}</code>.
@@ -14,6 +14,6 @@
     {:else}
         <h3 class="text-center text-mewd-white text-2xl">You have discovered a cool feature! Run <code style="color: rgb(147,128,24);">.dragon</code> to get some beta commands!</h3>
     {/if}
-  <h4 class="fw-bold text-center"><br><br><br>Do NOT, and I repeat, do NOT, give this code to anyone else. As
+  <h4 class="fw-bold text-center text-mewd-white"><br><br><br>Do NOT, and I repeat, do NOT, give this code to anyone else. As
     depending on how the bot is setup, it can be used to impersonate you.</h4>
 </div>
