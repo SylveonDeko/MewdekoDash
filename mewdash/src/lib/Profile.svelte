42211066b24c397825265f4b473181b5def85ff2
cat <lumitydev@gmail.com>
2023-02-02 10:40:32 -0600

    bug fixes

--- mewdash/src/lib/Profile.svelte
+++ mewdash/src/lib/Profile.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  export let profileData : {alt: string, image: string, name: string, description: string}
+  export let profileData : {alt: string, image: string, name: string, description: string, eggy: string}
 </script>
 
 <div class="flex flex-col items-center text-mewd-white p-7">
@@ -9,5 +9,5 @@
                               src={profileData.image}
                               width="150">
   <p class="font-bold text-2xl">{profileData.name}</p>
-  <p class="mb-2">{profileData.description}</p>
+  <p class="mb-2" id="profile-desc-{profileData.name}" onClick="var st='{profileData.eggy}'!=='undefined'?'{profileData.eggy}':'{profileData.description}';document.getElementById('profile-desc-{profileData.name}').innerText=st">{profileData.description}</p>
 </div>
