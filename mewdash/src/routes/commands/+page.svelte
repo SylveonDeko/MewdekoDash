7ce08407a6d17a8afedad0dcde0f85e1cc216a54
SylveonDeko <crimsonslayer76@gmail.com>
2023-02-06 16:23:08 -0500

    REEEEEEEEEEEEEEEE

---
+++ mewdash/src/routes/commands/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import modules from "./modules.json"
+</script>
+<ul>
+    {#each modules as module}
+        <li>{module.Name}
+            <ul>
+                {#each modules.Commands as command}
+                    <li>{command.CommandName}</li>
+                {/each}
+            </ul>
+        </li>
+    {/each}
+</ul>
