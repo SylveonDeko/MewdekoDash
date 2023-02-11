754eacda64fd5b7a1ecfb23ec3d8d5d6110d5fbc
Dominik Lenz <dominik@notabot.cc>
2023-02-11 22:57:29 +0100

    adds better cursors for details

--- mewdash/src/lib/dashboard/modules/MewdekoCommand.svelte
+++ mewdash/src/lib/dashboard/modules/MewdekoCommand.svelte
@@ -8,8 +8,8 @@
     export let command: Command;
 </script>
 
-<details class="group list-inside bg-mewd-dark-grey rounded-lg cursor-pointer break-normal">
-    <summary class="flex flex-row justify-start w-full py-3 px-5">
+<details class="group list-inside bg-mewd-dark-grey rounded-lg break-normal">
+    <summary class="flex flex-row justify-start w-full py-3 px-5 cursor-pointer">
         <h3 class="text-mewd-yellow text-sm font-bold">{capitalizeFirstLetter(command.CommandName)}</h3>
         <h4 class="group-open:hidden px-4 text-sm whitespace-nowrap text-ellipsis overflow-hidden"> {command.Description}</h4>
     </summary>
--- mewdash/src/lib/dashboard/modules/MewdekoModule.svelte
+++ mewdash/src/lib/dashboard/modules/MewdekoModule.svelte
@@ -6,7 +6,7 @@
     export let searching: boolean;
 </script>
 <details class="text-2xl font-bold text-mewd-white m-2" bind:open={searching}>
-    <summary>{module.Name}</summary>
+    <summary class="cursor-pointer">{module.Name}</summary>
     <div class="mt-4 flex flex-wrap font-semibold">
         {#each module.Commands as command, i}
             <div class="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
