cfff7d12b6e1d251bafabca167ac16286ca8d9a8
Dominik Lenz <dominik@notabot.cc>
2023-03-27 01:07:33 +0200

    fixes problem with old require syntax caused by mdsvex using prismjs

--- mewdash/src/routes/commands/+page.server.ts
+++ mewdash/src/routes/commands/+page.server.ts
@@ -1,8 +1,9 @@
 import type {PageServerLoad} from "./$types";
-import {compile} from "mdsvex";
 import type {Module} from "$lib/types/mewdekoModules";
-
+import showdown from "showdown";
 import json from "./modules.json"
+
+const converter = new showdown.Converter();
 
 export const load: PageServerLoad = async (): Promise<{ modules: Module[] }> => {
     let modules: Module[] = json
@@ -10,7 +11,7 @@ export const load: PageServerLoad = async (): Promise<{ modules: Module[] }> => 
     modules.forEach(module => {
         module.Commands.sort((a, b) => a.CommandName < b.CommandName ? -1 : a.CommandName > b.CommandName ? 1 : 0)
         module.Commands.forEach(async (command) => {
-            command.HtmlDescription = await compile(command.Description.trim()).then((result) => result?.code ?? "")
+            command.HtmlDescription =  converter.makeHtml(command.Description.trim())
         })
     })
 
