2f63e27f1ca39e51a8f7d19eaa90ec0e9f69f236
sylveondeko <oksamit45@gmail.com>
2023-04-26 13:44:45 -0400

    e

--- mewdash/src/routes/api/discord/discordAuth.ts
+++ mewdash/src/routes/api/discord/discordAuth.ts
@@ -1,22 +1,28 @@
 import {PUBLIC_DISCORD_API_URL, PUBLIC_DISCORD_REDIRECT_URI, PUBLIC_DISCORD_SCOPES} from "$env/static/public";
 import {DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET} from "$env/static/private";
 import type {Cookies} from "@sveltejs/kit";
+import {redirect} from "@sveltejs/kit";
 
 const ACCESS_TOKEN_COOKIE = "discord_access_token",
     REFRESH_TOKEN_COOKIE = "discord_refresh_token";
 
-export const requestDiscordToken = async (searchParams: URLSearchParams): Promise<Tokens> => {
+export const requestDiscordToken = async (searchParams: URLSearchParams, cookies: Cookies): Promise<Tokens | any> => {
     // performing a Fetch request to Discord's token endpoint
     const request = await fetch(`${PUBLIC_DISCORD_API_URL}/oauth2/token`, {
         method: 'POST',
         body: searchParams,
         headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     })
-
     const response = await request?.json()
 
+    if (request.status == 403)
+    {
+        deleteCookies(cookies)
+        return redirect(302, '/api/discord/login')
+    }
+
     if (response.error) {
-        throw  response.error
+        throw response.error
     }
 
     // redirect user to front page with cookies set
