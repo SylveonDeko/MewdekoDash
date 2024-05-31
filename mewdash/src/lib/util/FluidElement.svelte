e8d562f89b8b8e15fc8eb8631e436861c23ea7de
domlen2003 <37578299+domlen2003@users.noreply.github.com>
2024-05-31 17:17:21 +0200

    Update FluidElement.svelte
    - fix fluid element not showing properly on build

--- mewdash/src/lib/util/FluidElement.svelte
+++ mewdash/src/lib/util/FluidElement.svelte
@@ -7,6 +7,12 @@
     const widths = Array.from(Array(Math.min(maxBreaks - 1, sizes.length, breakpoints.length)).keys()) //maxBreaks - 1 because of 0 index
         .map((i) => `${breakpoints[i]}:w-${sizes[i]}`)  //Go  and map that to ascending splits
         .join(" ") //join with space
+
+    /* DONT REMOVE THIS, ITS FOR TAILWIND RETAINING CLASSES
+        md:w-1/2 md:w-1/3 md:w-1/4 md:w-1/5
+        lg:w-1/2 lg:w-1/3 lg:w-1/4 lg:w-1/5
+        xl:w-1/2 xl:w-1/3 xl:w-1/4 xl:w-1/5
+    */
 </script>
 
 <div class="w-full {widths} flex justify-center">
